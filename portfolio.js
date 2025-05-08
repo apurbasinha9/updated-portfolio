04d60f2d23b089110c20f8261202233ac021de76
Apurba Sinha <apurbasinha@Apurbas-MacBook-Air.local>
2025-05-08 00:24:42 -0230

    portfolio update completed

--- portfolio.js
+++ portfolio.js
@@ -8,6 +8,13 @@ let investopedia = document.getElementById("investopedia");
 let fpf = document.getElementById("fpf");
 let retrevium = document.getElementById("retrevium");
 let navbar = document.getElementById("navbar");
+let cBar = document.getElementsByClassName("c-bar");
+let pTag = document.querySelectorAll(".right p");
+let leftTitle = document.getElementById("left-title");
+let contactTitle = document.getElementById("contact-title");
+let left = document.getElementById("left");
+let messageButton = document.getElementById("message-button");
+let container = document.getElementById("container");
 
 function showMenu() {
   navList.classList.toggle("active");
@@ -65,4 +72,19 @@ retrevium.addEventListener("click", () => {
 if (window.innerWidth <= 768) {
   navbar.style.background = "#f0e9f1";
   navList.style.background = "#f0e9f1";
+}
+
+pTag.forEach((p) => {
+  p.style.marginBottom = "10px";
+});
+
+if (window.innerWidth <= 768) {
+  leftTitle.style.display = "none";
+  contactTitle.style.fontSize = "6vw";
+  contactTitle.textContent = "Contact me";
+  contactTitle.style.textAlign = "center";
+  left.style.display = "none";
+  messageButton.style.fontSize = "3vw";
+  messageButton.style.width = "100%";
+  container.style.paddingBottom = "0";
 }
